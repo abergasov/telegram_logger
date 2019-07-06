288bf9bc3b27b6e31478e847495393c8ce78ec3e
alejandro <abergasov@gmail.com>
2019-07-06 22:22:57 +0300

    init conf test and setup

--- src/Tlogger/TelegramLogger.php
+++ src/Tlogger/TelegramLogger.php
@@ -1,16 +1,65 @@
 <?php
 
 namespace Tlogger;
+use InvalidArgumentException;
 
 class TelegramLogger {
 
     private $token = null;
+    private $chatTarget = null;
+    private $logPath = null;
+    private $decorateUrl = null;
 
-    public function __construct($token) {
-        $this->setConfig();
+    /**
+     * TelegramLogger constructor.
+     * @param string $token bot token
+     * @param mixed $chatData target chat for messages. Or array of chats
+     * @param mixed $createTelegramLog should create file with log trace to telegram message. if set log path = trace log will be created there
+     * @param string $decorateUrl url to acess to log via internet. for example https://example.com/trace/logs/
+     */
+    public function __construct($token, $chatData, $createTelegramLog = false, $decorateUrl = '') {
+        if (!is_string($token)) {
+            throw new InvalidArgumentException('Token should be string');
+        }
+        $this->token = $token;
+        if (is_numeric($chatData)) {
+            $this->chatTarget = [$chatData];
+        } elseif (is_string($chatData)) {
+            $chatData = (int) $chatData;
+            if ($chatData === 0) {
+                throw new InvalidArgumentException('Chat data should be an integer or array of integers');
+            }
+            $this->chatTarget = [$chatData];
+        } elseif (is_array($chatData)) {
+            foreach ($chatData as $v) {
+                if (is_array($v)) throw new InvalidArgumentException('Multidimensional arrays not supported for chat data');
+            }
+            $this->chatTarget = $chatData;
+        } else {
+            throw new InvalidArgumentException('Chat data should be an integer or array of integers');
+        }
+        if (is_bool($createTelegramLog)) {
+            $this->decorateUrl = '';
+        } else {
+            if (!is_dir($createTelegramLog)) throw new InvalidArgumentException('Trace path does not exist');
+            if (!is_writable($createTelegramLog)) throw new InvalidArgumentException('Trace path is not writable');
+        }
+        $this->decorateUrl = $decorateUrl;
+        $this->logPath = $createTelegramLog;
     }
 
-    private function setConfig () {
-        $this->token = '';
+    public function __get($name) {
+        switch ($name) {
+            case 'token':
+                return $this->token;
+            case 'chatTarget':
+                return $this->chatTarget;
+            case 'logPath':
+                return $this->logPath;
+            case 'decorateUrl':
+                return $this->decorateUrl;
+            default:
+                return null;
+        }
     }
 }
--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -6,14 +6,76 @@ use Tlogger\TelegramLogger;
 class BaseTest extends TestCase {
 
     public function testConfigLoader () {
+        $token = '123';
+        $chatArray = [123];
+        $logger = new TelegramLogger($token, $chatArray);
+        $this->assertEquals($logger->token, $token);
 
-        $logger = new TelegramLogger(2);
+        //check target chat configured
+        $logger = new TelegramLogger($token, $chatArray);
+        $chatTarget = $logger->chatTarget;
+        $this->assertIsArray($chatTarget);
+        $this->assertTrue(count($chatTarget) > 0);
+        $chatArray = [
+            0 => 123,
+            1 => 222
+        ];
+        $logger = new TelegramLogger($token, $chatArray);
+        $chatTarget = $logger->chatTarget;
+        $this->assertIsArray($chatTarget);
+        $this->assertTrue(count($chatTarget) > 0);
 
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
+        //check path is validated
+        $createTelegramLog = false; //do not create file with trace logs
+        $logger = new TelegramLogger($token, $chatArray, $createTelegramLog);
+        $this->assertFalse($logger->logPath);
 
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        $createTelegramLog = true; //create file with trace logs
+        $logger = new TelegramLogger($token, $chatArray, $createTelegramLog);
+        $this->assertTrue($logger->logPath);
+
+        $createTelegramLog = __DIR__; //create file with trace logs
+        $decorateUrl = '';
+        $logger = new TelegramLogger($token, $chatArray, $createTelegramLog, $decorateUrl);
+        $this->assertIsString($logger->logPath);
+        $this->assertIsString($logger->decorateUrl);
+        $this->assertTrue(strlen($logger->decorateUrl) === 0);
+
+        $createTelegramLog = __DIR__; //create file with trace logs
+        $decorateUrl = 'asdsadsa';
+        $logger = new TelegramLogger($token, $chatArray, $createTelegramLog, $decorateUrl);
+        $this->assertIsString($logger->logPath);
+        $this->assertIsString($logger->decorateUrl);
+        $this->assertTrue(strlen($logger->decorateUrl) === strlen($decorateUrl));
+    }
+
+    public function testWrongTokenType () {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Token should be string');
+        new TelegramLogger(123, 123);
+    }
+
+    public function testWrongChatDataType () {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Chat data should be an integer or array of integers');
+        new TelegramLogger('123', 'test');
+    }
+
+    public function testMultidimensionArrays () {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Multidimensional arrays not supported for chat data');
+        new TelegramLogger('123', [[123]]);
+    }
+
+    public function testDirPathExist () {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Trace path does not exist');
+        new TelegramLogger('123', [123], 'test strnig');
+    }
+
+    public function testDirPathWritable () {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Trace path is not writable');
+        new TelegramLogger('123', [123], '/root');
     }
 }
